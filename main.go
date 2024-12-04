95df1fb5c60d76356e38183771a3562820636b76
lepc1972 <githublepc@gmail.com>
2024-12-04 16:56:08 -0500

    remaining tickets fixed

--- main.go
+++ main.go
@@ -14,4 +14,23 @@ func main() {
 	//fmt.Println("Welcome to our confrence booking app")
 	fmt.Println("Get your tickets here to attend")
 
+	//ask user for their name
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	println("Please insert your name")
+	fmt.Scan(&firstName) //pointer
+	println("Please insert your last name")
+	fmt.Scan(&lastName) //pointer
+	println("Please insert your email")
+	fmt.Scan(&email) //pointer
+	println("Please insert number of tickets")
+	fmt.Scan(&userTickets) //pointer
+	//println(userName)
+	//println(&userName)
+	remainingTickets = remainingTickets - uint(userTickets)
+
+	fmt.Printf("Thanks  %v %v for booking %v tickets. you will receive an email confirmation at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }

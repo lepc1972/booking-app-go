f62fa76c3d8ba7810f7a08c02c15d24e87d476b2
lepc1972 <githublepc@gmail.com>
2024-12-04 18:30:10 -0500

    infinite for loop

--- main.go
+++ main.go
@@ -14,23 +14,34 @@ func main() {
 	//fmt.Println("Welcome to our confrence booking app")
 	fmt.Println("Get your tickets here to attend")
 
-	//ask user for their name
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	println("Please insert your name")
-	fmt.Scan(&firstName) //pointer
-	println("Please insert your last name")
-	fmt.Scan(&lastName) //pointer
-	println("Please insert your email")
-	fmt.Scan(&email) //pointer
-	println("Please insert number of tickets")
-	fmt.Scan(&userTickets) //pointer
-	//println(userName)
-	//println(&userName)
-	remainingTickets = remainingTickets - uint(userTickets)
+	var bookings []string //slice, abstraction of array
 
-	fmt.Printf("Thanks  %v %v for booking %v tickets. you will receive an email confirmation at %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	//for loop infinite
+	for {
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+		println("Please insert your name")
+		fmt.Scan(&firstName) //pointer
+		println("Please insert your last name")
+		fmt.Scan(&lastName) //pointer
+		println("Please insert your email")
+		fmt.Scan(&email) //pointer
+		println("Please insert number of tickets")
+		fmt.Scan(&userTickets) //pointer
+		//println(userName)
+		//println(&userName)
+		remainingTickets = remainingTickets - uint(userTickets)
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thanks  %v %v for booking %v tickets. you will receive an email confirmation at %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+
+	}
+
+	//ask user for their name
+
 }
